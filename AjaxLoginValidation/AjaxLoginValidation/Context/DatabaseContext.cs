2769df11ef5d95ca1b57b153be3e900cd530a88e
Aimee Craig <aimee.craig@bglgroup.co.uk>
2019-03-15 15:18:50 +0000

    Project initialised for GitHub.

---
+++ AjaxLoginValidation/AjaxLoginValidation/Context/DatabaseContext.cs
@@ -0,0 +1,15 @@
+﻿using AjaxLoginValidation.Context;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AjaxLoginValidation.Context
+{
+    public class DatabaseContext(DbContextOptions<DatabaseContext> options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+}
